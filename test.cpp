dffdb1a295df874fcd76465ead3eff2e21bcaf05
Xiaosong <80614841+TianXieXiao@users.noreply.github.com>
2021-03-21 21:58:48 +0800

    Add files via upload
    C与C++在实现一个栈时C++满足C语言在封装性、初始化与删除、同时定义多个不同类型的栈的不足。

--- test.cpp
+++ test.cpp
@@ -1,44 +1,56 @@
 #include <iostream>
 using namespace std;
-struct ListNode_c
+typedef int STDateType;
+struct Stack_c
 {
-	int _val;
-	struct ListNode_c* _next;
-	struct ListNode_c* _prev;
-
+	STDateType* _a;
+	int _size;
+	int _capacity;
 };
-typedef struct ListNode_c ListNode_c;
-ListNode_c* BuyListNode_c(int val)
+typedef struct Stack_c Stack_c;
+void Stack_cInit(Stack_c* ps_c){}
+void Stack_cPush(Stack_c* ps_c, STDateType x){}
+void Stack_cPop(Stack_c* ps_c){}
+void Stack_cDestory(Stack_c* ps_c){}
+template<class T>
+class Stack_cpp
 {
-    ListNode_c* node_c = (ListNode_c*)malloc(sizeof(ListNode_c));
-	node_c->_val = 1;
-	node_c->_next = NULL;
-	node_c->_prev = NULL;
-	return node_c;
-}
-struct ListNode_cpp
-{
-	int _val;
-	struct ListNode_cpp* _next;//兼容C struct的用法
-	ListNode_cpp* _prev;//CPP中，struct已经可以认为是class类，区别是默认访问限定符
-	ListNode_cpp(int val = 0)
-		:_val(val)
-		, _next(nullptr)
-		, _prev(nullptr)
-	{}
-
+public:
+	Stack_cpp(){}
+	~Stack_cpp(){}
+	void push(T x){}
+private:
+	T* _a;
+	int _size;
+	int _capacity;
 };
 int main()
 {
-	/*int* p1 = (int*)malloc(sizeof(int));
-	int* p2 = new int;*/
-	ListNode_c* node1= BuyListNode_c(1);
-	ListNode_c* node2= BuyListNode_c(2);
-	ListNode_c* node3= BuyListNode_c(3);
+	//1.忘记初始化和销毁  ->s构造函数+析构函数
+	//2.没有封装，谁都可以修改   ->类+访问限定符
+	//3.如果想同时定义两个栈，做不到   ->模板
+	Stack_c st_c;
+	Stack_cInit(&st_c);
+	Stack_cPush(&st_c, 1);
+	Stack_cPush(&st_c, 2);
+	Stack_cPush(&st_c, 3);
+	Stack_cPush(&st_c, 4);
 	
-	ListNode_cpp *node4 = new ListNode_cpp;
-	ListNode_cpp *node5 = new ListNode_cpp(5);
+	//非法修改
+	st_c._capacity = 0;
 
+	Stack_cDestory(&st_c);
+	Stack_cpp <int>st_cpp_int;
+	st_cpp_int.push(1);        //实际也是两个参数，有一个隐含的this指针
+	st_cpp_int.push(2);
+	st_cpp_int.push(3);
+	st_cpp_int.push(4);
+	//st_cpp_int._capacity = 0;
+    Stack_cpp <double>st_cpp_double;
+	st_cpp_double.push(1.1);
+	st_cpp_double.push(1.2);
+	st_cpp_double.push(1.3);
+	st_cpp_double.push(1.4);
 
 	return 0;
 }
