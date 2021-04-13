dcf72ac6aa7cff46fe31d9a0dedcbf07402797c8
Xiaosong <80614841+TianXieXiao@users.noreply.github.com>
2021-04-13 22:39:00 +0800

    Add files via upload

---
+++ DivideInteger.c
@@ -0,0 +1,18 @@
+ #pragma warning(disable:4996) 
+#include <stdio.h>
+//递归方式实现打印一个整数的每一位
+void print(int n)
+{
+	if (n>9)
+	{
+		print(n / 10);
+	}
+	printf("%d ", n % 10);
+}
+int main()
+{
+	int n = 0;
+	scanf("%d", &n);
+	print(n);
+	return 0;
+}
