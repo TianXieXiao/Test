1ec5a7f1d803664215e203688ed06bfedc2eb6e0
Xiaosong <80614841+TianXieXiao@users.noreply.github.com>
2021-04-14 23:39:02 +0800

    Add files via upload

--- test.c
+++ test.c
@@ -1,35 +1,50 @@
  #pragma warning(disable:4996) 
 #include <stdio.h>
-#include <string.h>
-//逆置字符串内容
-void reverse(char* str, int n)
+//使用指针打印数组内容
+void PrintArray(int* p, int n)
 {
-	char *left = str;
-	char *right = str + n - 1;
-	while (left < right)
+	int i = 0;
+	for (; i < n; i++)
 	{
-		char tmp = *left;
-		*left = *right;
-		*right = tmp;
+		printf("%d ", *(p + i));
 	}
-}
-int strlen(char* str)
-{
-	int count = 0;
-	while (*str != '\0')
-	{
-		count++;
-		str++;
-	}
-	return count;
 }
 int main()
 {
-	char str[] = "abcdef";
-	printf("逆置前的字符串为：%s\n",str);
-	int n = strlen(str);
-	printf("%d", n);
-	reverse(str, n);
-	printf("逆置后的字符串为：%s\n", str);
+	int arr[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 0 };
+	int n = sizeof(arr) / sizeof(arr[0]);
+	PrintArray(arr, n);
 	return 0;
 }
+//#include <string.h>
+//void ReverseString(char *p,int n)
+//{
+//	char* left = p;
+//	char* right = p +n-1;
+//	while (left < right)
+//	{
+//		char tmp = *left;
+//		*left = *right;
+//		*right = tmp;
+//		left++; right--;
+//	}
+//}
+//int Strlen(char *p)
+//{
+//	int count = 0;
+//	while ((*p) != '\0')
+//	{
+//		count++;
+//		p++;
+//	}
+//	return count;
+//}
+//int main()
+//{
+//	char str[]= "abcdef";
+//	int n = Strlen(str);
+//	printf("逆置前的字符串：%s\n", str);
+//	ReverseString(str,n);
+//	printf("逆置后的字符串：%s\n", str);
+//	return 0;
+//}
