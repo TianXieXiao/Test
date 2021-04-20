1f1005a763d50920399fd6365c60463cf3134ee3
Xiaosong <80614841+TianXieXiao@users.noreply.github.com>
2021-04-20 22:27:06 +0800

    Add files via upload

--- 4-4_SuShu.c
+++ 4-4_SuShu.c
@@ -34,30 +34,30 @@
 //	return 0;
 //}
 
-//#include <stdio.h>
-//int is_prime(int i)
-//{
-//	int j = 0;
-//	for (j = 2; j < i; ++j)
-//	{
-//		if (i%j == 0)
-//			return 0;
-//	}
-//	return 1;
-//
-//}
-//int main()
-//{
-//	int i = 0;
-//	for (i = 100; i <= 200; ++i)
-//	{
-//		if (is_prime(i) == 1)
-//		{
-//			printf("%d\t", i);
-//		}
-//	}
-//	return 0;
-//}
+#include <stdio.h>
+int is_prime(int i)
+{
+	int j = 0;
+	for (j = 2; j < i; ++j)
+	{
+		if (i%j == 0)
+			return 0;
+	}
+	return 1;
+
+}
+int main()
+{
+	int i = 0;
+	for (i = 100; i <= 200; ++i)
+	{
+		if (is_prime(i) == 1)
+		{
+			printf("%d\t", i);
+		}
+	}
+	return 0;
+}
 //#include <stdio.h>
 //void Year(int year)
 //{
