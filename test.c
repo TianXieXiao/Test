756d0db371ecab6b803f33bc60d7415498637be9
Xiaosong <80614841+TianXieXiao@users.noreply.github.com>
2021-05-06 23:34:54 +0800

    Add files via upload

--- test.c
+++ test.c
@@ -1,218 +1,114 @@
  #pragma warning(disable:4996) 
+//反向输出4位数
 #include <stdio.h>
-//输入一个这整数，判断这个数中9的个数
-//int is_prime(int n)
-//{
-//	int count = 0;
-//	while (n)
-//	{
-//		if (n % 10 == 9)
-//			count++;
-//		n /= 10;
-//	}
-//	return count;
-//}
-//int main()
-//{
-//	int i = 0;
-//	scanf("%d", &i);
-//	printf("%d\n", is_prime(i));
-//	return 0;
-//}
-//找出10个整数中的最大值
-//方法一：利用ar[]开辟动态数组》》》》》》》》》》》》》》》》》》》》》》》》》》》》》》》》》》》1
-//int main()
-//{
-//	int i = 0;
-//	int ar[] = { 0 };
-//	while (ar[i] != '\n')
-//	{
-//		scanf("%d", &ar[i++]);
-//	}
-//	
-//	//while (ar[i++] != '\0')
-//	//{
-//	//	scanf("%d", ar[i]);
-//
-//	//}
-//	//scanf("%d", ar[i++]);
-//	printf("%d", i);
-//	return 0;
-//}
-//提前开好空间时间复杂度为O（1）
-//int main()
-//{	
-//	int ar[] = {-1,-2,-3,-4,-5,-6,-7,-8,-9,-23};
-//	int max = ar[0];
-//	for (int i = 1; i < 10; i++)
-//	{
-//		if (max < ar[i])
-//			max = ar[i];
-//	}
-//	printf("%d\n", max);
-//	return 0;
-//}
-//利用calloc()函数开辟动态数组空间求其中最大的整数
-//#include <stdlib.h>
-//int main()
-//{
-//	int i, n;
-//	int* pdata;
-//	printf("请问您准备存储多少个数据:");
-//	scanf("%d", &i);
-//	pdata = (int*)calloc(i, sizeof(int));
-//	printf("请输入：");
-//	for (n = 0; n < i; ++n)
-//	{
-//		scanf("%d", &pdata[n]);
-//	}
-//	printf("您已经存储了以下%d个数据: ", i);
-//	for (n = 0; n < i; ++n)
-//	{
-//		printf("%d ", pdata[n]);
-//	}
-//		printf("\n");
-//	int max = pdata[0];
-//	for (n = 1; n < i; n++)
-//	{
-//		if (max < pdata[n])
-//			max = pdata[n];
-//	}
-//	printf("您输入的数据中%d最大！\n", max);
-//	free(pdata);
-//	return 0;
-//}
-////请输入一个范围，并打印该范围内的素数以及个数
-//int is_prime(int b, int e)
-//{
-//	int i = 2;
-//	int count = 0;
-//	for (b; b <= e; b++)
-//	{
-//		if (b >5)
-//		{
-//			for (i; i < b / 2; i++,count)
-//			{
-//				if (b%i == 0)
-//				{
-//					break;
-//				}
-//				else
-//				{
-//					printf("%d", i);
-//				}
-//			}
-//		}
-//		
-//		else if (b=2)
-//		{
-//			printf("%d", b);
-//			count++;
-//		}
-//		else if (b>2&&b<=5)
-//		{
-//			for (i; i < b; i++,count++)
-//			{
-//				if (b%i == 0)
-//					break;
-//				printf("%d", i);
-//			}
-//		}
-//	}
-//	return count;
-//}
-//int main()
-//{
-//	int begin = 0;
-//	int end = 0;
-//	while (1)   //判断输入的区间是否在自然数中求质数的合理区间[2,n]内
-//	{
-//		scanf("%d%d", &begin, &end);     
-//		if (begin > end|| begin<=1)
-//		{
-//			printf("请输入正确的范围：");
-//		}
-//		else
-//		{
-//			break;
-//		}	
-//	}
-//	int a = 6;
-//	int b = 10;
-//	int c = is_prime(a, b);
-////	printf("该范围内的素数有%d",c );
-////	return 0;
-//	/*do
-//	{
-//		switch (juDge(begin, end))
-//		{
-//		case 0:
-//			scanf("%d-%d", &begin, &end);
-//			break;
-//		case 1:
-//			printf("请输入正确的范围~:");
-//
-//		default:
-//		}
-//	} while (begin > end);*/
-//	
-//}
-////
-//int main()
-//{
-//	int i, j;
-//	for (i = 100; i <= 200; ++i)
-//	{
-//		for (j = 2; j < i; ++j)
-//		{
-//			if (i%j == 0)
-//				break;
-//		
-//		}
-//		if (j==i)
-//		printf("%d", i);
-//	}
-//	return 0;
-//}
-//void  DivideInteger(int n)
-//{
-//	if (n > 9)
-//	{
-//		DivideInteger(n / 10);
-//	}
-//	printf("%d  ",n % 10);	
-//}
-//int main()
-//{
-//	int i = 0;
-//	scanf("%d", &i);
-//	printf("%d的每个数字分别是:",i);
-//	DivideInteger(i);
-//	return 0;
-//}
-//int Factorial(int n)
-//{
-//	if (n <= 1)
-//		return 1;
-//	else
-//		return n*Factorial(n - 1);
-//}
-//int main()
-//{
-//	int n = 0;
-//	scanf("%d", &n);
-//	printf("%d的阶乘为：%d\n", n,Factorial(n));
-//	return 0;
-//}
-//int main()
-//{
-//	int i = 0;
-//	int ret = 1;
-//	scanf("%d", &i);
-//	for (int n = 1; n <= i; ++n)
-//	{
-//		ret *= n;
-//	}
-//	printf("%d的阶乘为：%d", i, ret);
-//	return 0;
-//}
+int main()
+{
+	//计算一个整数的商和余数
+	//int a = 0;
+	//int b = 0;
+	//scanf("%d %d", &a, &b);
+	//int merchant = a / b;//商
+	//int remainder = a%b;//余数
+	//printf("%d %d", merchant, remainder);
+	//计算表达式的值
+	/*int a = 40;
+	int c = 212;
+	int sum = (-8 + 22)*a - 10 + c / 2;
+	printf("%d", sum);*/
+	//输出对应字符的ASCII码
+	//char ch = 0;
+	//scanf("%c", &ch);
+	//printf("%d\n", ch);//错误点，直接char = 0;&char,char了
+	//void swap(int *a, int *b)
+	//{
+	//	int tmp = *a;
+	//	*a = *b;
+	//	*b = tmp;
+	//}
+	//int a = 0;
+	//int b = 0;
+	//scanf("a=%d,b=%d", &a, &b);//swap函数可以实现，输入的格式可以这样，牛客上不能多引入不相关的头文件
+	////int tmp = a;
+	////a = b;
+	////b = tmp;
+	//swap(&a, &b);
+	//printf("a=%d,b=%d\n", a, b);
+	//用循环多组输出整型，输出为2的n次方，不用累乘
+	/*int ch = 0;
+	while ((scanf("%d", &ch)) != EOF)
+	{
+		printf("%d\n", 1 << ch);
+	}*/
+	//本地的难点在于如何按照指定域宽读取数据，如何按照指定格式打印数据。
+	//int year = 0;
+	//int month = 0;
+	//int date = 0;
+	//scanf("%4d%2d%2d", &year, &month, &date);//%4d就是读取4位整数
+	//printf("year=%d\n", year);
+	//printf("month=%02d\n", month);//输出不够2位时左边拿0补充
+	//printf("date=%02d\n", date);
+	//char a[] = { 73, 32, 99, 97, 110, 32, 100, 111, 32, 105, 116, 33 };//int 和char 都可以
+	//int i = 0;
+	////计算数组元素个数
+	//int sz = sizeof(a) / sizeof(a[0]);
+	////循环遍历输出每个元素
+	//for (i = 0; i<sz; i++)
+	//{
+	//	printf("%c", a[i]);//每个字符不要隔开
+	//}
+	//char n = 0;
+	//scanf("%c", &n);//输入的是字符金字塔
+	//int i = 0;
+	//for (i = 0; i<5; i++)
+	//{
+	//	//处理一行
+	//	//空格
+	//	int j = 0;
+	//	for (j = 0; j<5 - i - 1; j++)
+	//	{
+	//		printf(" ");
+	//	}
+	//	//字符
+	//	for (j = 0; j <= i; j++)
+	//	{
+	//		printf("%c ", n);//字符后面的空格易忘记，输出格式应该也为字符
+	//	}
+	//	printf("\n");
+	//}
+		//int ch = 0;
+		//while ((ch = getchar()) != EOF)
+		//{
+		//	if ((ch >= 'a' && ch <= 'z') || (ch >= 'A' &&ch <= 'Z'))//容易敲错或者忘掉
+		//		printf("YES\n");
+		//	else
+		//		printf("NO\n");
+		//	getchar();
+		//}
+	//int StuId = 0;
+	//float Clanguage = 0.0;
+	//float Math = 0.0;
+	//float English = 0.0;
+	//scanf("%d;%f,%f,%f", &StuId, &Clanguage, &Math, &English);//注意输入的格式
+	//printf("The each subject score of  No. %d is %.2f, %.2f, %.2f.", //注意%2f与%.2f的区别
+	//	StuId, Clanguage, Math, English);
+	//printf("\n%d\n", printf("Hello world!"));
+	//int ret = printf("Hello world!");//printf函数返回值是字符串长度，调用的同时并返回一个值
+	//printf("\n");
+	//printf("%d\n", ret);
+	//printf("%15d\n", 0XABCDEF);
+	//大小写转换
+	//int ch = 0;
+	//while ((ch = getchar())!= EOF)
+	//{
+	//	printf("%c\n",(ch+32));
+	//	getchar();//行缓冲区，再次调用吸收掉\n字符
+	//}
+	/*int n = 0;
+	scanf("%d", &n);
+	while (n)
+	{
+		printf("%d ", n % 10);
+		n /= 10;
+	}*/
+	return 0;
+}
