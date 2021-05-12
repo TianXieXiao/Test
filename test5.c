452be66687c52012467e271c0b40fe023c5fea4e
Xiaosong <80614841+TianXieXiao@users.noreply.github.com>
2021-05-12 23:40:08 +0800

    Add files via upload

--- test5.c
+++ test5.c
@@ -1,21 +1,45 @@
- #pragma warning(disable:4996) 
+
 #include <stdio.h>
-int Count(int x)
+
+// 如果想要通过修改形参来达到对外部实参的修改
+// 必须要传递实参的地址
+void Swap1(int* left, int* right)
 {
-	int count = 0;
-	while (x)
-	{
-		if ((x & 1) == 1)
-			count++;
-		x >>= 1;
-	}
-	return count;
+	printf("%p\n", left);
+	printf("%p\n", right);
+
+	int temp = *left;
+	*left = *right;
+	*right = temp;
 }
+
+void Swap2(int** left, int** right)
+{
+	printf("%p\n", left);
+	printf("%p\n", right);
+
+	int* temp = *left;
+	*left = *right;
+	*right = temp;
+}
+
+
 int main()
 {
-	int x = 0;
-	scanf("%d", &x);
-	int ret = Count(x);
-	printf("%d", ret);
+	int a = 10;
+	int b = 20;
+
+
+	int* pa = &a;
+	int* pb = &b;
+
+	printf("%p\n", &a);
+	printf("%p\n", &b);
+
+	// 如果实参是值类型，函数的参数只需为一级指针即可
+	Swap1(&a, &b);
+
+	// 如果实参是一级指针，函数的形参必须为二级指针
+	Swap2(&pa, &pb);   // pa->b   pb->a
 	return 0;
 }
