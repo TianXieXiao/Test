1dbc2b62f29c3095079851b5decd230eb29fa1a9
Xiaosong <80614841+TianXieXiao@users.noreply.github.com>
2021-05-13 23:00:49 +0800

    Add files via upload

--- test.c
+++ test.c
@@ -1,45 +1,9 @@
-
 #include <stdio.h>
-
-// 如果想要通过修改形参来达到对外部实参的修改
-// 必须要传递实参的地址
-void Swap1(int* left, int* right)
-{
-	printf("%p\n", left);
-	printf("%p\n", right);
-
-	int temp = *left;
-	*left = *right;
-	*right = temp;
-}
-
-void Swap2(int** left, int** right)
-{
-	printf("%p\n", left);
-	printf("%p\n", right);
-
-	int* temp = *left;
-	*left = *right;
-	*right = temp;
-}
-
-
 int main()
 {
-	int a = 10;
-	int b = 20;
-
-
-	int* pa = &a;
-	int* pb = &b;
-
-	printf("%p\n", &a);
-	printf("%p\n", &b);
-
-	// 如果实参是值类型，函数的参数只需为一级指针即可
-	Swap1(&a, &b);
-
-	// 如果实参是一级指针，函数的形参必须为二级指针
-	Swap2(&pa, &pb);   // pa->b   pb->a
+	int a = 40;
+	int c = 212;
+	int ep = (-8 + 22)*a - 10 + c/2;
+	printf(" %d\n", ep);
 	return 0;
 }
