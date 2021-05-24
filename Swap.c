54308fb2802ac094121bf3a16ae7ea864000b230
Xiaosong <80614841+TianXieXiao@users.noreply.github.com>
2021-05-24 22:25:17 +0800

    Add files via upload

---
+++ Swap.c
@@ -0,0 +1,72 @@
+//
+//#include <stdio.h>
+//void Swap(int* x, int* y)
+//{
+//	int* tmp = x;
+//	x = y;
+//	y = tmp;
+//}
+//int main()
+//{
+//	int* x = 0;
+//	int* y = 0;
+//	scanf("%d %d", &x, &y);
+//	Swap(&x, &y);
+//	printf("%d %d", x, y);
+//	return 0;
+//}
+//#define _CRT_SECURE_NO_WARNINGS
+//#include<stdio.h>
+//#include<stdlib.h>
+//
+//void ChangeNum(int* x, int* y) {
+//	int temp = 0;
+//	temp = *x;
+//	*x = *y;
+//	*y = temp;
+//}
+//
+//int main() {
+//	int* a = 0;
+//	int* b = 0;
+//	printf("输入两个数啊a,b:\n");
+//	scanf("%d %d", &a, &b);
+//	ChangeNum(&a, &b);
+//	printf("a=%d\nb=%d\n", a, b);
+//	return 0;
+//}
+#pragma warning(disable:4996) 
+#include <stdio.h>
+// void MultiMulti(int* k)
+//{
+//	int sum = 0, i = 0, j = 0;
+//	for (i = 1; i <= *k; ++i)
+//	{
+//		for (j = 1; j <= i; ++j)
+//		{
+//			sum = i*j;
+//			printf("%d*%d=%d  ", j, i, sum);
+//		}
+//		printf("\n");
+//	}	
+//}
+//int main()
+//{
+//	int k = 0;
+//	scanf("%d", &k);
+//	MultiMulti(&k);
+//	return 0;
+//}
+void PriIntNum(int a)
+{
+	= a % 10;
+
+	if ()
+}
+int main()
+{
+	int a = 0;
+	scanf("%d", &a);
+	PriIntNum(a);
+	return 0;
+}
