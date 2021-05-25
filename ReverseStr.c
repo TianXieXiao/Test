f8c23b53ef36f3bbcdb2ea330c5413e009fcd694
Xiaosong <80614841+TianXieXiao@users.noreply.github.com>
2021-05-25 22:15:21 +0800

    Add files via upload

---
+++ ReverseStr.c
@@ -0,0 +1,36 @@
+#pragma warning(disable:4996)
+#include <stdio.h>
+
+
+void reverse(char* str, int n)
+{
+	char *left = str;
+	char *right = str + n - 1;
+	while (left < right)
+	{
+		char tmp = *left;
+		*left = *right;
+		*right = tmp;
+		left++; right--;
+	}
+}
+int strlen(char* str)
+{
+	int count = 0;
+	while (*str != '\0')
+	{
+		count++;
+		str++;
+	}
+	return count;
+}
+int main()
+{
+	char str[] = "abcdef";
+	printf("ÄæÖÃÇ°µÄ×Ö·û´®Îª£º%s\n", str);
+	int n = strlen(str);
+	printf("%d", n);
+	reverse(str, n);
+	printf("ÄæÖÃºóµÄ×Ö·û´®Îª£º%s\n", str);
+	return 0;
+}
