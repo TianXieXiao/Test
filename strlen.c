c02125dbb262574f553454431ea770406f317c23
Xiaosong <80614841+TianXieXiao@users.noreply.github.com>
2021-05-25 22:16:18 +0800

    Add files via upload

--- strlen.c
+++ strlen.c
@@ -1,68 +1,36 @@
  #pragma warning(disable:4996) 
-//模拟实现strlen库函数
 #include <stdio.h>
-#include <string.h>
-#include <assert.h>
-//用递归实现strlen
-size_t my_strlen(const char *str)
-{
-	assert(str != NULL);
-	if (*str == '\0')
-		return 0;
-	else
-		return my_strlen(str + 1) + 1;//如果用str++就会造成死循环
-}
-//用指针相减实现strlen
-//size_t my_strlen(const char *str)
+#include <windows.h>
+//非递归求字符串的长度
+//int Strlen(char* arr)
 //{
-//	assert(str != NULL);
-//	const char *tmp = str;
-//	while (*tmp != '\0')
-//	{
-//		tmp++;
-//	}
-//	return tmp - str;
-//}
-//用for()循环实现的strlen
-//size_t my_strlen(const char *str)
-//{
-//	assert(str != NULL);
 //	int i = 0;
-//	for( i=0 ; *str!= '\0'; i++)
+//	while (*arr != '\0')
 //	{
-//		str++;
+//		++arr;
+//		++i;
 //	}
 //	return i;
 //}
-//用while()循环实现的strlen
-//size_t my_strlen(const char *str)
+//int main()
 //{
-//	assert(str != NULL);//参数的有效性检查
-//	const char *tmp = str;//保护参数
-//	int count = 0;
-//	while (*tmp != '\0')
-//	{
-//		count++;
-//		tmp++;
-//	}
-//	return count;
+//	char arr[] = "abcdef";
+//	printf("%d\n", Strlen(arr));
+//	system("pause");
+//	return 0;
 //}
-void print(char *a)
+//递归求字符串的长度
+int strlen(char* arr)
 {
-	while (*a != '\0')
-	{
-		printf("%c ", *a++);
-	}
+	if (*arr == '\0')
+		return 0;
+	else
+		return strlen(++arr) + 1;
 }
 int main()
 {
-	char *a = "abcdef";
-	char b[] = "abc\0def";
-	char c[] = { 'a', 'b', 'c', 'd','\0' };//字符数组遇到'\0'就结束
-	//int len = strlen(a);
-	//printf("%d\n", len);
-	size_t ret = my_strlen(a);
-	printf("%d\n", ret);
-	//print(a);
+	char arr[] = "abcdefg";
+	printf("%d", strlen(arr));
+	system("pause");
 	return 0;
 }
