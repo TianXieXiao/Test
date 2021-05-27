24b175154d533f32bc3a2ca1aa65d598f58df749
Xiaosong <80614841+TianXieXiao@users.noreply.github.com>
2021-05-27 23:06:54 +0800

    Add files via upload

---
+++ SeqListOj.c
@@ -0,0 +1,42 @@
+ #pragma warning(disable:4996) 
+#include <stdio.h>
+#include <assert.h>
+#include <stdlib.h>
+/**
+* Definition for singly-linked list.
+* struct ListNode {
+*     int val;
+*     struct ListNode *next;
+* };
+*/
+
+
+struct ListNode* removeElements(struct ListNode* head, int val){
+	struct ListNode *cur = head;
+	struct ListNode *prev = NULL;
+
+	while (cur)
+	{
+		if (cur->val == val)
+		{
+			if (NULL == prev)
+			{
+				head = cur->next;
+				free(cur);
+				cur = head;
+			}
+			else
+			{
+				prev->next = cur->next;
+				free(cur);
+				cur = prev->next;
+			}
+		}
+		else
+		{
+			prev = cur;
+			cur = cur->next;
+		}
+	}
+	return head;
+}
